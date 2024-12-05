8529aa18ac277ef8ff6f2ffdaf4c43f9cf92b2f7
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-06 01:42:05 +0530

    first commit

---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+class ForLoop{
+    public static void main(String[] args) {
+        int [] a={12,18,16,71};
+        for(int i =0;i<a.length;i++){
+            System.out.println(a[i]);
+        }    
+    }
+}
