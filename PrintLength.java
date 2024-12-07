eb192ca88c9c390df0ae6396003cb4cec85cc383
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-07 15:36:39 +0530

    Solving more problems

---
+++ PrintLength.java
@@ -0,0 +1,8 @@
+public class PrintLength {
+    public static void main(String[] args) {
+        int [] a={12,18,30,40};
+        int [] b =new int[10];
+        System.out.println(a.length);
+        System.out.println(b.length);
+    }
+}
