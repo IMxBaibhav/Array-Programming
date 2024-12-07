7eaaaf7dfb2a64ad59c68dc7ae5d52cfc1e5ce48
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-07 15:39:44 +0530

    String also

--- PrintLength.java
+++ PrintLength.java
@@ -2,6 +2,8 @@ public class PrintLength {
     public static void main(String[] args) {
         int [] a={12,18,30,40};
         int [] b =new int[10];
+        String s1 = "mohan is here";
+        System.out.println(s1.length());
         System.out.println(a.length);
         System.out.println(b.length);
     }
