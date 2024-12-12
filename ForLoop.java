5c351e0cec95a8b9a134195676762e772987a76f
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-12 23:53:40 +0530

    nnn

--- ForLoop.java
+++ ForLoop.java
@@ -1,8 +1,8 @@
 class ForLoop{
     public static void main(String[] args) {
-        int [] a={12,18,16,71};
+        int [] b={12,18,16,71};
         for(int i =0;i<a.length;i++){
-            System.out.println(a[i]);
+            System.out.println(b[i]);
         }    
     }
 }
