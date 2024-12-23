13713c9bf5059399cbc5a5a5975b11f5bc8dd12d
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-23 16:16:06 +0530

    Solving more problems

--- ForLoop.java
+++ ForLoop.java
@@ -1,6 +1,6 @@
 class ForLoop{
     public static void main(String[] args) {
-        int [] a={12,18,16,71};
+        int [] b={12,18,16,71};
         for(int i =0;i<a.length;i++){
             System.out.println(a[i]);
         }    
