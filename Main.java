e579817dfa11a9aeeb9518308d66a9677833f64e
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-25 23:09:10 +0530

    minor change

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@
 
 // Person Class
 class Person {
-    // Encapsulating the name and age
-    // only approachable and used using
-    // methods defined
+    
     private String name;
     private int age;
 
