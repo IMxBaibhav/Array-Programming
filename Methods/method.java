5b7b860e0958e350b9ad94ad6040fd413b185978
lexendx <tiwaribaibhav2004@gmail.com>
2026-04-10 11:55:30 +0530

    Methods

---
+++ Methods/method.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+
+public class method{
+    public static void main(String[] args) {
+        sum();}
+
+        static void sum(){
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the number 1");
+        int num1 = in.nextInt();
+        System.out.println("Enter the number 2");
+        int num2 = in.nextInt();
+        int sum= num1+num2;
+        System.out.println(sum);    
+
+
+        }
+    }
+
+
